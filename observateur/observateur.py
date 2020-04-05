5b0522d8cc1d127a33086072dce84e9ee394648a
DamdlBT <56141589+DamdlBT@users.noreply.github.com>
2020-04-05 15:09:28 -0400

    Envoi des actions au serveur

--- observateur/observateur.py
+++ observateur/observateur.py
@@ -1,4 +1,7 @@
+import requests
+
 log = "latest.log"
+URL = "http://172.105.0.226:5000"
 
 def read_log(n, log=log):
 
@@ -18,21 +21,29 @@ def read_log(n, log=log):
 
     return status
 
+
+def post_actions(actions):
+
+    requests.post(URL, json=actions)
+
+
 status_prec = {}
 
-for i in range(262):
+for i in range(0, 262, 10):
     status = read_log(i)
     if status_prec != status:
+        actions = []
         for player in status:
             if player in status_prec:
                 if status[player] != status_prec[player]:
                     if status[player]:
-                        print(player + " Joined")
+                        # print(player + " Joined")
+                        actions.append({"name": player, "action": "join"})
                     else:
-                        print(player + " Left")
+                        # print(player + " Left")
+                        actions.append({"name": player, "action": "left"})
             else:
-                print(player + " Joined")
-
-        print()
+                actions.append({"name": player, "action": "join"})
+        post_actions(actions)
 
     status_prec = status
